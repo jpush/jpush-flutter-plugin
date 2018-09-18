6edcc0eb3ea30f1d732fa49f7a2b3d4a76db9242
huangminlinux <380108184@qq.com>
2018-09-18 16:43:49 +0800

    adjust jpush.dart to jpush_flutter.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:flutter/services.dart';
-import 'package:jpush/jpush.dart';
+import 'package:jpush_flutter/jpush_flutter.dart';
 
 void main() => runApp(new MyApp());
 
