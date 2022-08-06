5ba1f47a955a2246a569c55f5574463f3f323240
Jiankian <luozhengan@vip.qq.com>
2022-08-07 02:29:02 +0800

    Android project upgrade

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\Workspace\android\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Workspace\flutter\jpush-flutter-plugin\example"
+export "FLUTTER_ROOT=/Users/jiankian/Applications/Developer/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/jiankian/AndroidStudioProjects/jpush-flutter-plugin/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=false"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.packages"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
