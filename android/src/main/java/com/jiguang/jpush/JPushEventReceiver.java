27cf4f9ffc822918221710a62f69db3d427e7894
Lcterry <lx798465@gmail.com>
2023-09-21 12:05:23 +0800

    更新JPush 520

--- android/src/main/java/com/jiguang/jpush/JPushEventReceiver.java
+++ android/src/main/java/com/jiguang/jpush/JPushEventReceiver.java
@@ -17,10 +17,10 @@ import java.util.Set;
 import cn.jpush.android.api.JPushInterface;
 import cn.jpush.android.api.JPushMessage;
 import cn.jpush.android.api.NotificationMessage;
-import cn.jpush.android.service.JPushMessageReceiver;
+import cn.jpush.android.service.JPushMessageService;
 import io.flutter.plugin.common.MethodChannel.Result;
 
-public class JPushEventReceiver extends JPushMessageReceiver {
+public class JPushEventReceiver extends JPushMessageService {
 
     @Override
     public void onNotifyMessageUnShow(Context context,final NotificationMessage notificationMessage) {
