bd1df92afdf527b4d9fa3b1d27ad62d4a4c17a0c
huangshuni <15889638723@163.com>
2024-12-26 16:51:57 +0800

    修复 iOS 杀死app点击通知不走回调的问题

--- ios/Classes/JPushPlugin.m
+++ ios/Classes/JPushPlugin.m
@@ -577,6 +577,11 @@ static NSMutableArray<FlutterResult>* getRidResults;
         }
         _launchNotification = localNotificationEvent;
     }
+    
+    JPUSHRegisterEntity * entity = [[JPUSHRegisterEntity alloc] init];
+    entity.types = JPAuthorizationOptionAlert;
+    [JPUSHService registerForRemoteNotificationConfig:entity delegate:self];
+    
     //[self performSelector:@selector(addNotificationWithDateTrigger) withObject:nil afterDelay:2];
     return YES;
 }
@@ -657,28 +662,7 @@ static NSMutableArray<FlutterResult>* getRidResults;
     JPLog(@"application:didReceiveRemoteNotification:fetchCompletionHandler");
     JPLog(@"UNUserNotificationCenter.delegate:%@  applidelegate:%@",[UNUserNotificationCenter currentNotificationCenter].delegate, [UIApplication sharedApplication].delegate);
     [JPUSHService handleRemoteNotification:userInfo];
-    if (@available(* ,iOS 10)) {
-        [_channel invokeMethod:@"onReceiveNotification" arguments:userInfo];
-       
-        /**
-         * 下面这段代码是解决 app处于杀死状态,点击通知启动app,但是不回调onOpenNotification的问题。
-         * 上诉情况不会走didReceiveNotificationResponse：回调。但是会走didReceiveRemoteNotification:fetchCompletionHandler:回调。iOS原生项目中正常情况下，点击通知冷启动app是会回调didReceiveNotificationResponse，不回调didReceiveRemoteNotification:fetchCompletionHandler:的。
-         * 因为不走didReceiveNotificationResponse：回调 所以没有onOpenNotification回调。这跟生命周期有关，didReceiveNotificationResponse:的代理需要通知的远程代理设置要在didFinishLaunch结束之前。但是flutter初始化jpush是在didFinishLaunch之后。
-         * 在这个方法里做一个判断吧，如果收到的消息和启动时的消息是同一个消息，则判断该消息为app杀死状态下通过点击通知唤醒的。
-         */
-        JPLog(@"didReceiveRemoteNotification:%@ - %@", _launchNotification, userInfo);
-        if (_launchNotification && userInfo && [_launchNotification isKindOfClass:[NSDictionary class]] && [userInfo isKindOfClass:[NSDictionary class]]) {
-            // 拿到启动时的推送数据里的msgid
-            NSNumber *launchMsgid = [_launchNotification valueForKey:@"_j_msgid"];
-            // 拿到收到的消息的msgid
-            NSNumber *msgid = [userInfo valueForKey:@"_j_msgid"];
-            // 如果消息id一致
-            if ([launchMsgid isKindOfClass:[NSNumber class]] && [msgid isKindOfClass:[NSNumber class]] && [[launchMsgid stringValue] isEqualToString:[msgid stringValue]]) {
-                JPLog(@"didReceiveRemoteNotification:消息id一致，回调通知点击");
-                [_channel invokeMethod:@"onOpenNotification" arguments:_launchNotification];
-            }
-        }
-    }
+    [_channel invokeMethod:@"onReceiveNotification" arguments:userInfo];
     completionHandler(UIBackgroundFetchResultNewData);
     return YES;
 }
@@ -725,11 +709,11 @@ static NSMutableArray<FlutterResult>* getRidResults;
     if (!self.unShow) completionHandler(notificationTypes);
 }
 
-- (void)jpushNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)())completionHandler  API_AVAILABLE(ios(10.0)){
+- (void)jpushNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)(void))completionHandler  API_AVAILABLE(ios(10.0)){
     NSDictionary * userInfo = response.notification.request.content.userInfo;
     JPLog(@"jpushNotificationCenter:didReceiveNotificationResponse::%@",userInfo);
     if([response.notification.request.trigger isKindOfClass:[UNPushNotificationTrigger class]]) {
-        JPLog(@"iOS10 点击远程通知");
+        JPLog(@"iOS10 点击远程通知 %@",_channel);
         [JPUSHService handleRemoteNotification:userInfo];
         [_channel invokeMethod:@"onOpenNotification" arguments: [self jpushFormatAPNSDic:userInfo]];
     }else{
